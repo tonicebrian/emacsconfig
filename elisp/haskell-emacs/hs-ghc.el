da301db5fc571906b1116f3676a5086a9478f3d9
Toni Cebrián <ancechu@gmail.com>
2013-03-17 19:58:26 +0100

    Delete everything to start afresh

--- elisp/haskell-emacs/hs-ghc.el
+++
@@ -1,8 +0,0 @@
-(defun hs-ghc-version ()
-  "Get the version of the GHC we're using."
-  (let* ((out (shell-command-to-string (format "%s --version" hs-config-ghci-bin)))
-         (match (string-match "version \\([0-9\.]+\\)$" out)))
-    (when match
-      (match-string 1 out))))
-
-(provide 'hs-ghc)
