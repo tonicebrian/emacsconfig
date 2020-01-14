f8170e5fa3e606641f28e02d375238c6cfd2eeab
Toni Cebrián <toni.cebrian@dowjones.com>
2020-01-14 15:36:06 +0100

    Specify where to put the info box

--- init.el
+++ init.el
@@ -190,6 +190,7 @@ inhibit-startup-echo-area-message t)
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(lsp-ui-doc-position (quote top))
  '(lsp-ui-flycheck-enable t)
  '(lsp-ui-sideline-enable t)
  '(org-agenda-files (quote ("~/Dropbox/GTD/gtd.org")))
